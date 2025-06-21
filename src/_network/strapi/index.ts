e84beb6139c090198307a2b25a25fbc7a9a52ed5
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-21 19:25:24 +0100

    feat: faqs. #2

---
+++ src/_network/strapi/index.ts
@@ -0,0 +1,17 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+
+export const strapiClient = new ApolloClient<NormalizedCacheObject>({
+  link: new HttpLink({
+    uri: process.env.STRAPI_SCHEMA_URL,
+    fetch,
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+  }),
+  cache: new InMemoryCache(),
+});
