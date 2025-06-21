01efd86c9b7eb218a8f6b2434fd4d8b725910e61
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-21 19:07:31 +0100

    feat: faqs. #2

--- src/_core/uikit/index.ts
+++ src/_core/uikit/index.ts
@@ -1,8 +1,9 @@
 // Components
 export * from './components/Button';
-export * from './components/Icon';
-export * from './components/ThemeProvider';
 export * from './components/Heading';
+export * from './components/Icon';
+export * from './components/Row';
+export * from './components/ThemeProvider';
 
 // Assets
 export { IMAGES } from './assets/images';
--- src/app/(pages)/faq/page.tsx
+++ src/app/(pages)/faq/page.tsx
@@ -1,18 +1,41 @@
 import { Metadata } from 'next';
 import { FC, Fragment } from 'react';
+import { faker } from '@faker-js/faker';
 
-import { Heading } from '@core/uikit';
+import { Heading, Row } from '@core/uikit';
+
+const getFaqs = async (): Promise<
+  {
+    id: string;
+    title: string;
+    description: string;
+  }[]
+> => {
+  return new Promise((resolve) => {
+    resolve(
+      Array.from({
+        length: 9,
+      }).map(() => ({
+        id: faker.string.uuid(),
+        title: faker.lorem.words(3),
+        description: faker.lorem.sentences(3),
+      })),
+    );
+  });
+};
 
 const Faq: FC = async () => {
+  const data = await getFaqs();
+
   return (
     <Fragment>
       <Heading>
         <Heading.Title>FAQs</Heading.Title>
       </Heading>
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="row justify-content-center mt-3">
-            <div className="col-xl-5 col-lg-8">
+      <Row>
+        <Row.Col lg={12}>
+          <Row className="justify-content-center mt-3">
+            <Row.Col xl={5} lg={8}>
               <div className="text-center">
                 <h5>Can't find what you are looking for?</h5>
                 <p className="text-muted">
@@ -34,139 +57,32 @@ const Faq: FC = async () => {
                     Send us a tweet
                   </button>
                 </div>
-
-                <div className="row justify-content-center">
-                  <div className="col-xl-10">
-                    <form className="app-search d-none d-lg-block mt-4">
-                      <div className="position-relative">
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Search..."
-                        />
-                        <button className="btn btn-primary" type="button">
-                          <i className="bx bx-search-alt align-middle"></i>
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
               </div>
-            </div>
-          </div>
-
+            </Row.Col>
+          </Row>
+        </Row.Col>
+      </Row>
+      <div className="row">
+        <div className="col-lg-12">
           <div className="row mt-5">
-            <div className="col-xl-4 col-sm-6">
-              <div className="card">
-                <div className="card-body overflow-hidden position-relative">
-                  <div>
-                    <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
+            {data.map(({ id, title, description }, index) => {
+              return (
+                <div className="col-xl-4 col-sm-6" key={id}>
+                  <div className="card">
+                    <div className="card-body overflow-hidden position-relative">
+                      <div>
+                        <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
+                      </div>
+                      <div className="faq-count">
+                        <h5 className="text-primary">{`0${index + 1}.`}</h5>
+                      </div>
+                      <h5 className="mt-3">{title}</h5>
+                      <p className="text-muted mt-3 mb-0">{description}</p>
+                    </div>
                   </div>
-                  <div className="faq-count">
-                    <h5 className="text-primary">01.</h5>
-                  </div>
-                  <h5 className="mt-3">What is Lorem Ipsum?</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    New common language will be more simple and regular than the
-                    existing European languages. It will be as simple as
-                    occidental.
-                  </p>
                 </div>
-              </div>
-            </div>
-
-            <div className="col-xl-4 col-sm-6">
-              <div className="card">
-                <div className="card-body overflow-hidden position-relative">
-                  <div>
-                    <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
-                  </div>
-                  <div className="faq-count">
-                    <h5 className="text-primary">02.</h5>
-                  </div>
-                  <h5 className="mt-3">Where does it come from?</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    Everyone realizes why a new common language would be
-                    desirable one could refuse to pay expensive translators.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-xl-4 col-sm-6">
-              <div className="card">
-                <div className="card-body overflow-hidden position-relative">
-                  <div>
-                    <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
-                  </div>
-                  <div className="faq-count">
-                    <h5 className="text-primary">03.</h5>
-                  </div>
-                  <h5 className="mt-3">Where can I get some?</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    If several languages coalesce, the grammar of the resulting
-                    language is more simple and regular than that of the
-                    individual languages.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-xl-4 col-sm-6">
-              <div className="card">
-                <div className="card-body overflow-hidden position-relative">
-                  <div>
-                    <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
-                  </div>
-                  <div className="faq-count">
-                    <h5 className="text-primary">04.</h5>
-                  </div>
-                  <h5 className="mt-3">Why do we use it?</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    Their separate existence is a myth. For science, music,
-                    sport, etc, Europe uses the same vocabulary.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-xl-4 col-sm-6">
-              <div className="card">
-                <div className="card-body overflow-hidden position-relative">
-                  <div>
-                    <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
-                  </div>
-                  <div className="faq-count">
-                    <h5 className="text-primary">05.</h5>
-                  </div>
-                  <h5 className="mt-3">Where can I get some?</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    The point of using Lorem Ipsum is that it has a more-or-less
-                    normal they distribution of letters opposed to using content
-                    here.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-xl-4 col-sm-6">
-              <div className="card">
-                <div className="card-body overflow-hidden position-relative">
-                  <div>
-                    <i className="bx bx-help-circle widget-box-1-icon text-primary"></i>
-                  </div>
-                  <div className="faq-count">
-                    <h5 className="text-primary">06.</h5>
-                  </div>
-                  <h5 className="mt-3">What is Lorem Ipsum?</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    To an English person, it will seem like simplified English,
-                    as a skeptical Cambridge friend of mine told me what
-                    Occidental
-                  </p>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
