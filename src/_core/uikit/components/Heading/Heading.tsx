9cc86ceccc8b27ec50c8bb7b376430230f65b0cc
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-23 23:20:05 +0100

    feat: faqs. #2

--- src/_core/uikit/components/Heading/Heading.tsx
+++ src/_core/uikit/components/Heading/Heading.tsx
@@ -1,7 +1,13 @@
 import { PropsWithChildren, ReactElement } from 'react';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
 
 import { HeadingTitle } from './HeadingTitle';
 import { HeadingBreadcrumb } from './HeadingBreadcrumb';
+
+const chevron = icon(faChevronRight).html[0];
+console.log(chevron);
+const chevronBase64 = Buffer.from(chevron).toString('base64');
 
 export type HeadingProps = PropsWithChildren;
 
@@ -14,7 +20,14 @@ interface HeadingComponent {
 export const Heading: HeadingComponent = ({ children }) => {
   return (
     <div className="row">
-      <div className="col-12">
+      <div
+        className="col-12"
+        style={
+          {
+            '--bs-breadcrumb-divider': `url("data:image/svg+xml;base64,${chevronBase64}")`,
+          } as React.CSSProperties
+        }
+      >
         <div className="page-title-box d-sm-flex align-items-center justify-content-between">
           {children}
         </div>
