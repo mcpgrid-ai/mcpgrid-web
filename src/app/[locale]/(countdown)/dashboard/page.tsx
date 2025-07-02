974f400f294edb589223dd2e434ca84f65c73e84
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-02 19:36:35 +0100

    feat: dashboard. #15

--- src/app/[locale]/(countdown)/dashboard/page.tsx
+++ src/app/[locale]/(countdown)/dashboard/page.tsx
@@ -1,7 +1,43 @@
 import { FC, Fragment } from 'react';
 
 const DashboardSoon: FC = () => {
-  return <Fragment>Coming soon</Fragment>;
+  return (
+    <Fragment>
+      <div className="mb-5">
+        <a href="index.html">
+          <img
+            src="assets/images/logo-sm.svg"
+            alt=""
+            height="30"
+            className="me-1"
+          />
+          <span className="logo-txt text-white font-size-22">
+            {process.env.PRODUCT_NAME}
+          </span>
+        </a>
+      </div>
+      <h3 className="text-white mt-5">Let's get started with Minia</h3>
+      <p className="text-white-50 font-size-16">
+        Donec pede justo fringilla vel aliquet nec vulputate eget arcu. In enim
+        justo, rhoncus ut imperdiet a venenatis vitae, justo felis
+      </p>
+
+      <div data-countdown="2023/12/31" className="counter-number mt-5"></div>
+
+      <form className="app-search mt-5 mx-auto">
+        <div className="position-relative">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your email address"
+          />
+          <button className="btn btn-primary" type="button">
+            <i className="bx bx-paper-plane align-middle"></i>
+          </button>
+        </div>
+      </form>
+    </Fragment>
+  );
 };
 
 export default DashboardSoon;
--- src/app/[locale]/(countdown)/layout.tsx
+++ src/app/[locale]/(countdown)/layout.tsx
@@ -1,23 +1,38 @@
-'use client';
+import { FC, Fragment, PropsWithChildren } from 'react';
 
-import { FC, PropsWithChildren } from 'react';
-
-import { useTheme } from '@core/uikit';
+import BG_IMG from './layout.jpg';
+import './layout.scss';
 
 type CountdownLayoutProps = PropsWithChildren;
 
 const CountdownLayout: FC<CountdownLayoutProps> = ({ children }) => {
-  const { mode } = useTheme();
-
   return (
-    <body
-      data-layout-size="boxed"
-      data-layout="horizontal"
-      data-topbar={mode}
-      data-bs-theme={mode}
-    >
-      {children}
-    </body>
+    <Fragment>
+      <div className="preview-img overflow-hidden ">
+        <div className="swiper-container preview-thumb">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">
+              <div
+                className="slide-bg"
+                style={{ backgroundImage: `url(${BG_IMG.src})` }}
+                // style="background-image: url(./assets/images/bg-1.jpg);"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="coming-content min-vh-100 py-4 px-3 py-sm-5">
+        <div className="bg-overlay bg-primary"></div>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="text-center py-4 py-sm-5">{children}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
