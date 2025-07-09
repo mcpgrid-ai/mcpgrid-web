13d9fbe68d6a792abe84b90919db3f3822265ccf
Andrii Maiskyi <andrey@wearehelful.com>
2025-07-10 00:14:01 +0100

    feat: search. #18

--- src/app/[locale]/(pages)/servers/_partitions/ServersSearch/ServersSearch.tsx
+++ src/app/[locale]/(pages)/servers/_partitions/ServersSearch/ServersSearch.tsx
@@ -3,13 +3,18 @@
 import { FC } from 'react';
 import { createPortal } from 'react-dom';
 
-import { GlobalSearch } from '@common/components';
-import { DomNodeId } from '@common/constants/domNodeId.const';
+import { ServersSearch as Search } from '@common/components';
+import { DomNodeId } from '@common/constants';
 
 export const ServersSearch: FC = () => {
   const el = document.getElementById(DomNodeId.HeaderSearch);
 
   if (!el) return null;
 
-  return createPortal(<GlobalSearch />, el);
+  return createPortal(
+    <div className="w-75">
+      <Search className="border" />
+    </div>,
+    el,
+  );
 };
